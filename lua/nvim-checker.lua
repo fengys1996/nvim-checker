1fec5fa7d5caeb0bc3c1a9e5d0704e1537e4bca2
fys <1113014250@qq.com>
2022-08-29 22:51:41 +0800

    fix refresh

--- lua/nvim-checker.lua
+++ lua/nvim-checker.lua
@@ -2,7 +2,7 @@ local function fmt()
   vim.cmd("vsplit | terminal")
   local command = ':call jobsend(b:terminal_job_id, "cargo fmt --all\\n")'
   vim.cmd(command)
-  local refresh = ':e!'
+  local refresh = ':bufdo e!'
   vim.cmd(refresh)
 end
 
