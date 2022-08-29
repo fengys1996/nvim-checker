62a5f0c3a97b2f592117ca34d66ce5aba9820db2
fys <1113014250@qq.com>
2022-08-29 23:07:13 +0800

    fix

--- lua/nvim-checker.lua
+++ lua/nvim-checker.lua
@@ -3,13 +3,13 @@ local function check()
   local format = ':call jobsend(b:terminal_job_id, "cargo fmt --all\\n")'
   vim.cmd(format)
 
-  local refresh = ':bufdo e!'
+  local refresh = ':bufdo e!\\n'
   vim.cmd(refresh)
 
-  local clippy = ':call jobsend(b:terminal_job_id, "cargo clippy --workspace --all-targets -- -D warnings -D clippy::print_stdout -D clippy::print_stderr")'
+  local clippy = ':call jobsend(b:terminal_job_id, "cargo clippy --workspace --all-targets -- -D warnings -D clippy::print_stdout -D clippy::print_stderr\\n")'
   vim.cmd(clippy)
 
-  local test = ':call jobsend(b:terminal_job_id, "cargo test --all")'
+  local test = ':call jobsend(b:terminal_job_id, "cargo test --all\\n")'
   vim.cmd(test)
 end
 
