8c46ab5460960a2bf96de7bbf03f4837d36acb4c
fys <1113014250@qq.com>
2022-08-29 23:21:15 +0800

    only support clippy now

--- lua/nvim-checker.lua
+++ lua/nvim-checker.lua
@@ -1,16 +1,7 @@
 local function check()
   vim.cmd("vsplit | terminal")
-  local format = ':call jobsend(b:terminal_job_id, "cargo fmt --all\\n")'
-  vim.cmd(format)
-
-  local refresh = ':bufdo e!\\n'
-  vim.cmd(refresh)
-
   local clippy = ':call jobsend(b:terminal_job_id, "cargo clippy --workspace --all-targets -- -D warnings -D clippy::print_stdout -D clippy::print_stderr\\n")'
   vim.cmd(clippy)
-
-  local test = ':call jobsend(b:terminal_job_id, "cargo test --all\\n")'
-  vim.cmd(test)
 end
 
 return {
