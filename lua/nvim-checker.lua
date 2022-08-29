1224b9b5690ace797520b685451d39edc0d704e7
fys <1113014250@qq.com>
2022-08-29 22:57:40 +0800

    try fmt refresh clippy test

--- lua/nvim-checker.lua
+++ lua/nvim-checker.lua
@@ -1,11 +1,18 @@
-local function fmt()
+local function check()
   vim.cmd("vsplit | terminal")
-  local command = ':call jobsend(b:terminal_job_id, "cargo fmt --all\\n")'
-  vim.cmd(command)
+  local format = ':call jobsend(b:terminal_job_id, "cargo fmt --all\\n")'
+  vim.cmd(format)
+
   local refresh = ':bufdo e!'
   vim.cmd(refresh)
+
+  local clippy = ':call jobsend(b:terminal_job_id, "cargo clippy --workspace --all-targets -- -D warnings -D clippy::print_stdout -D clippy::print_stderr")'
+  vim.cmd(clippy)
+
+  local test = ':call jobsend(b:terminal_job_id, "cargo test --all")'
+  vim.cmd(test)
 end
 
 return {
-  fmt = fmt
+  check = check
 }
