ca894a756d10538f1eefda6e92d8f31a53ffd40d
fys <1113014250@qq.com>
2022-10-15 10:27:56 +0800

    add command 'cargo clippy, check, test'

--- lua/nvim-checker.lua
+++ lua/nvim-checker.lua
@@ -1,9 +1,21 @@
-local function check()
-  vim.cmd("vsplit | terminal")
-  local clippy = ':call jobsend(b:terminal_job_id, "cargo clippy --workspace --all-targets -- -D warnings -D clippy::print_stdout -D clippy::print_stderr\\n")'
-  vim.cmd(clippy)
+module = {}
+
+function module.clippy()
+	vim.cmd("vsplit | terminal")
+	local clippy = ':call jobsend(b:terminal_job_id, "cargo clippy --workspace --all-targets -- -D warnings -D clippy::print_stdout -D clippy::print_stderr\\n")'
+	vim.cmd(clippy)
 end
 
-return {
-  check = check
-}
+function module.check()
+	vim.cmd("vsplit | terminal")
+	local check = ':call jobsend(b:terminal_job_id, "cargo check\\n")'
+	vim.cmd(check)
+end
+
+function module.test()
+	vim.cmd("vsplit | terminal")
+	local test = ':call jobsend(b:terminal_job_id, "cargo test --all\\n")'
+	vim.cmd(test)
+end
+
+return module
