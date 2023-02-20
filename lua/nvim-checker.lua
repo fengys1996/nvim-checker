6ec7bd47f80c9a3e4beed7099558387aa1bc9031
fys <1113014250@qq.com>
2023-02-20 22:46:31 +0800

    add screen_vertical config opt

--- lua/nvim-checker.lua
+++ lua/nvim-checker.lua
@@ -1,19 +1,31 @@
 module = {}
 
 function module.clippy()
-	vim.cmd("vsplit | terminal")
+	if vim.g.screen_vertical then
+		vim.cmd("split | terminal")
+	else
+		vim.cmd("vsplit | terminal")
+	end
 	local clippy = ':call jobsend(b:terminal_job_id, "cargo clippy --workspace --all-targets -- -D warnings -D clippy::print_stdout -D clippy::print_stderr\\n")'
 	vim.cmd(clippy)
 end
 
 function module.check()
-	vim.cmd("vsplit | terminal")
+	if vim.g.screen_vertical then
+		vim.cmd("split | terminal")
+	else
+		vim.cmd("vsplit | terminal")
+	end
 	local check = ':call jobsend(b:terminal_job_id, "cargo check\\n")'
 	vim.cmd(check)
 end
 
 function module.test()
-	vim.cmd("vsplit | terminal")
+	if vim.g.screen_vertical then
+		vim.cmd("split | terminal")
+	else
+		vim.cmd("vsplit | terminal")
+	end
 	local test = ':call jobsend(b:terminal_job_id, "cargo test --all\\n")'
 	vim.cmd(test)
 end
